463daf1a5f8f0cdaf5ac9ee1227c57073877e4db
Amarendraojha <amarendra.ojha@appinventiv.com>
2024-09-20 18:51:19 +0530

    Update ProductStorageEvery.jsx

--- src/SubPages.jsx/ProductStorageEvery.jsx
+++ src/SubPages.jsx/ProductStorageEvery.jsx
@@ -14,11 +14,17 @@ const ProductStorageEvery = () => {
 
                 {Product.map((Product,id) =>(
                 <div className='productmainEvery' key={id}>
-                     <nav><Link to='/ProductCount'><img className="productimgEvery"  src={Product.image} alt='image'/></Link></nav> 
+                     <nav>
+                         <Link to='/ProductCount'>
+                             <img className="productimgEvery"  src={Product.image} alt='image'/>
+                         </Link>
+                     </nav> 
                       <p>{Product.type}</p>
                       <h2 className="productnameheadEvery"> {Product.productname}</h2>
-                      <div className="starEvery"><img className="productimgstarEvery" src={Product.star} alt='image'/>
-                      <p className="priceparaEvery"> {Product.price}</p></div>
+                      <div className="starEvery">
+                          <img className="productimgstarEvery" src={Product.star} alt='image'/>
+                      <p className="priceparaEvery"> {Product.price}</p>
+                      </div>
                 </div>) )}
             
             </div>
